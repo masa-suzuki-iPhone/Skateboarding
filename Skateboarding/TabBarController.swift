6853637683014f143373c4ea6c30919053bf7aca
masa-suzuki-iPhone <sp1103masa@gmail.com>
2022-07-24 00:59:08 +0900

    Fix a bug that tab bar becomes transparent when returning to MapViewController from a screen transition destination

--- Skateboarding/TabBarController.swift
+++ Skateboarding/TabBarController.swift
@@ -13,27 +13,10 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // タブアイコンの色
-        self.tabBar.tintColor = UIColor.init(red: 0/255, green: 69/255, blue: 130/255, alpha: 1)
-        // タブバーの背景色
-        self.tabBar.barTintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
         // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
         self.delegate = self
     }
-    
-    // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する。
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        if viewController is ImageSelectViewController {
-            // ImageSelectViewControllerは、タブ切り替えではなくモーダル画面遷移する
-            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelectViewController")
-            present(imageSelectViewController, animated: true)
-            return false
-        } else {
-            // その他のViewControllerは通常のタブ切り替えを実施
-            return true
-        }
-    }
-    
+        
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -49,7 +32,18 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         }
     }
 
-
+    // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する。
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is ImageSelectViewController {
+            // ImageSelectViewControllerは、タブ切り替えではなくモーダル画面遷移する
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelectViewController")
+            present(imageSelectViewController, animated: true)
+            return false
+        } else {
+            // その他のViewControllerは通常のタブ切り替えを実施
+            return true
+        }
+    }
 
 
 
